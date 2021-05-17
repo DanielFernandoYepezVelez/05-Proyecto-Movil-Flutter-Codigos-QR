381a7099124197dc879ad869f05114de03d232d0
danielfernandoyepezvelez <danielfernandoyepezvelez>
2021-05-16 21:50:20 -0500

    Iniciando El Proyecto Lector De qr

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:qr_reader/providers/ui_provider.dart';
+
+import 'package:qr_reader/screens/home.dart';
+import 'package:qr_reader/screens/mapa.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => UiProvider())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'QR Reader',
+        initialRoute: 'home',
+        routes: {'home': (_) => HomePage(), 'mapa': (_) => MapaPage()},
+        theme: ThemeData(
+            primaryColor: Colors.deepPurple,
+            floatingActionButtonTheme: FloatingActionButtonThemeData(
+                backgroundColor: Colors.deepPurple)),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:qr_reader/providers/ui_provider.dart';
+
+import 'package:qr_reader/widgets/custom_navigator_bar.dart';
+import 'package:qr_reader/widgets/scan_button.dart';
+import 'package:qr_reader/screens/direcciones.dart';
+import 'package:qr_reader/screens/historial_mapas.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        title: Text('Historial'),
+        actions: [
+          IconButton(icon: Icon(Icons.delete_forever), onPressed: () {})
+        ],
+      ),
+      body: _HomePageBody(),
+      bottomNavigationBar: CustomNavigationBar(),
+      floatingActionButton: ScanButtom(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
+class _HomePageBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    /* Obtener El SelectedMenuOpt(Provider) */
+    final uiProvider = Provider.of<UiProvider>(context);
+
+    /* Cambiar Para Mostrar La Página Respectiva */
+    final currentIndex = uiProvider.selectedMenuOpt;
+
+    switch (currentIndex) {
+      case 0:
+        return HistorialMapasPage();
+
+      case 1:
+        return DireccionesPage();
+
+        break;
+      default:
+        return HistorialMapasPage();
+    }
+  }
+}
